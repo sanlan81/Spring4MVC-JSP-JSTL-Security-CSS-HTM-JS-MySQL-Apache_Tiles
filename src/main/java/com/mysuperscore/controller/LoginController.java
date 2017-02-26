176a0d7070d7e22232de620e0fbf396dedbfef1f
sanlan81 <sanlan81@gmail.com>
2017-02-26 23:36:13 +0200

    #2  changed funny var names like FEAR_COLOR to MEDIUM_COLOR

--- src/main/java/com/mysuperscore/controller/LoginController.java
+++ src/main/java/com/mysuperscore/controller/LoginController.java
@@ -11,23 +11,23 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/")
 public class LoginController {
     private static final int WEAK_STRENGTH = 1;
-    private static final int FEAR_STRENGTH = 5;
+    private static final int MEDIUM_STRENGTH = 5;
     private static final int STRONG_STRENGTH = 7;
 
     private static final String WEAK_COLOR = "#FF0000";
-    private static final String FEAR_COLOR = "#FF9900";
+    private static final String MEDIUM_COLOR = "#FF9900";
     private static final String STRONG_COLOR = "#006400";
 
     @RequestMapping(value = "/checkStrength",method = RequestMethod.GET,produces = {"text/html"})
     @ResponseBody
     public String checkStrength(@RequestParam String password){
         String result = "<span style=\"color:%s; font-weight:hold;\">%s</span>";
-         if(password.length() >= WEAK_STRENGTH & password.length() < FEAR_STRENGTH){
+         if(password.length() >= WEAK_STRENGTH & password.length() < MEDIUM_STRENGTH){
 
              return String.format(result,WEAK_COLOR,"WEAK");
-         }else if(password.length() >= FEAR_STRENGTH & password.length() < STRONG_STRENGTH){
+         }else if(password.length() >= MEDIUM_STRENGTH & password.length() < STRONG_STRENGTH){
 
-             return String.format(result,FEAR_COLOR,"MEDIUM");
+             return String.format(result,MEDIUM_COLOR,"MEDIUM");
          }else if(password.length() >= STRONG_STRENGTH){
 
              return String.format(result,STRONG_COLOR,"STRONG");
